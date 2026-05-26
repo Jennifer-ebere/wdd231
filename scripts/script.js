8730d2cf8b0573acf9833361eff2c1a8fa845818
Jennifer-ebere <jebere@byupathway.edu>
2026-05-26 13:36:59 +0100

    Week 4 Learning Activity

--- scripts/script.js
+++ scripts/script.js
@@ -20,6 +20,37 @@ const allBtn = document.getElementById("allBtn");
 const cseBtn = document.getElementById("cseBtn");
 const wddBtn = document.getElementById("wddBtn");
 
+const courseDetails = document.getElementById("courseDetails");
+
+// MODAL FUNCTION
+function displayCourseDetails(course) {
+
+  courseDetails.innerHTML = `
+  
+    <button id="closeModal">❌</button>
+
+    <h2>${course.subject} ${course.number}</h2>
+
+    <h3>${course.title}</h3>
+
+    <p><strong>Credits:</strong> ${course.credits}</p>
+
+    <p><strong>Certificate:</strong> ${course.certificate}</p>
+
+    <p>${course.description}</p>
+
+    <p><strong>Technologies:</strong> ${course.technology.join(", ")}</p>
+  
+  `;
+
+  courseDetails.showModal();
+
+  document.getElementById("closeModal").addEventListener("click", () => {
+    courseDetails.close();
+  });
+
+}
+
 // DISPLAY COURSES
 function displayCourses(courseList) {
 
@@ -37,18 +68,25 @@ function displayCourses(courseList) {
     if (course.completed) {
       card.classList.add("completed");
     }
+
     card.innerHTML = `
-  ${course.completed ? "✔️ " : ""}
-  ${course.subject} ${course.number}
-`;
+      ${course.completed ? "✔️ " : ""}
+      ${course.subject} ${course.number}
+    `;
+
+    // OPEN MODAL WHEN CARD IS CLICKED
+    card.addEventListener("click", () => {
+      displayCourseDetails(course);
+    });
 
     courseContainer.appendChild(card);
   });
 
-  // TOTAL CREDITS USING REDUCE
+  // TOTAL CREDITS
   const total = courseList.reduce((sum, course) => {
     return sum + course.credits;
   }, 0);
+
   creditTotal.textContent =
     `The total credits for the courses listed above is ${total}`;
 }
